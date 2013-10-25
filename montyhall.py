02de31aa687adb9b4dff75b809590532e70e0f34
Pavel Popov <pavelpopov@outlook.com>
2013-10-25 21:46:37 +0400

    modified:   montyhall.py Impoved performance: now it takes 0.07 sec per 10 iterations with 1000 elements and 3 doors

--- montyhall.py
+++ montyhall.py
@@ -1,78 +1,34 @@
 __author__ = 'Pavel Popov'
 
 import random
-#import matplotlib.pyplot as plt
+import sys
 
 
-def set_choices(doors=3):
-    correct_choice = -1
-    choices = [0 for i in range(doors)]
-    random.seed()
-    for i in range(doors):
-        if random.random() >= 1.0-1.0/float(doors):
-            choices[i] = 1
-            correct_choice = i
-            break
-    if correct_choice == -1:
-        choices[-1] = 1
-        correct_choice = doors-1
-    return (choices, correct_choice)
-
-
-def do_choice(doors=3):
-    random.seed()
-    return random.randint(0, doors-1)
-
-
-def remove_choices(choices, choice):
-    random.seed()
-
-    if choice == choices[1]:  # chosen correct choice
-        choice = 0
-    else:
-        choice = 1
-
-    return {'choices': [1,0], 'choice': choice, 'correct_choice': 0}
-
-
-def check_solution(choices, change):
+def check_solution(choice, change):
     change_f = lambda x: 1 if x == 0 else 0
-
     if change:
-        choices['choice'] = change_f(choices['choice'])
+        choice = change_f(choice)
+    return choice == 0
 
-    return choices['choice'] == choices['correct_choice']
 
+def montyhall(doors, change):
+    do_choice = lambda x: random.randint(0, x-1)
+    choice = 0 if do_choice(doors) == do_choice(doors) else 1
+    return 1 if check_solution(choice, change) else 0
 
-def montyhall(doors=3, change=True):
-    choices = set_choices(doors)
-    choice = do_choice(doors)
-    new_choices = remove_choices(choices, choice)
-    res = check_solution(new_choices, change)
-    return 1 if res else 0
-    # print check_solution(new_choices, True)
-
-def central_limit_theorem(series_length, num_of_series):
-    n = series_length
-    data = []
-    for i in range(num_of_series):
-        data.append([random.random() for x in range(n)])
-    sum_of_i = lambda i: sum([data[j][i] for j in range(num_of_series)])
-    data_sum = map(sum_of_i, range(n))
-
-    #plt.hist(data_sum, bins=500)
-    #plt.show()
-
-#if __name__ == '__main__':
-    # central_limit_theorem(50000, 50)
-    #print sum([montyhall(3,True) for i in range(1000)])
-    #print sum([montyhall(3,False) for i in range(1000)])
 
 if __name__ == '__main__':
-    import timeit
-    print(timeit.timeit("sum([montyhall(3,True) for i in range(1000)])",
-                        number=10,
+    if len(sys.argv) == 2 and sys.argv[1] == 'timeit':
+        import timeit
+        number=10
+        print(timeit.timeit("sum([montyhall(3,True) for i in range(1000)])",
+                            number=number,
+                            setup="from __main__ import montyhall"))
+        print(timeit.timeit("sum([montyhall(3,False) for i in range(1000)])",
+                            number=number,
                         setup="from __main__ import montyhall"))
-    print(timeit.timeit("sum([montyhall(3,False) for i in range(1000)])",
-                        number=10,
-                        setup="from __main__ import montyhall"))
+    else:
+        print('Changed - %d' % sum([montyhall(3,True) for i in range(1000)]));
+        print('Not Changed - %d' % sum([montyhall(3,False) for i in range(1000)]));
+
+
